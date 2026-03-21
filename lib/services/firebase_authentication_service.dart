e284ee2154944b29cfa56ff96abef2349d3b8011
Xolani <xolani@tosh.co.za>
2026-03-21 12:29:44 +0200

    Add firebase login

--- lib/services/firebase_authentication_service.dart
+++ lib/services/firebase_authentication_service.dart
@@ -3,7 +3,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<void> register(String email, String password) async {
-    await _auth.createUserWithEmailAndPassword(email: email, password: password);
+  Future<User?> register(String email, String password) async {
+    final result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    return result.user;
+  }
+
+  Future<User?> login(String email, String password) async {
+    final result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+    return result.user;
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+
+  User? getCurrentUser() {
+    return _auth.currentUser;
   }
 }
