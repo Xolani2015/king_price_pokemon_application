ce6978b0b986d903a587aa53e7204bafc375805f
Xolani <xolani@tosh.co.za>
2026-03-21 08:53:27 +0200

    add linting file

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:king_price_pokemon_application/views/registration/registration_page.dart';
 
 void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const RegistrationPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
