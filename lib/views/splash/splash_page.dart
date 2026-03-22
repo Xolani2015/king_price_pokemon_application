db7dcfe02c643b4c28a08dee7ead7a903c4d14cc
Xolani2015 <xr.mkhwanazi.20@gmail.com>
2026-03-22 17:37:39 +0200

    Update Splash Screen

--- lib/views/splash/splash_page.dart
+++ lib/views/splash/splash_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:king_price_pokemon_application/helpers/app_colors.dart';
 import 'package:king_price_pokemon_application/helpers/app_enums.dart';
+import 'package:king_price_pokemon_application/helpers/app_sizes.dart';
 import 'package:king_price_pokemon_application/widgets/app_template.dart';
 import 'package:king_price_pokemon_application/views/login/login_page.dart'; // <-- import your login page
 
@@ -14,29 +16,23 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 0), () {
+    Future.delayed(const Duration(seconds: 5), () {
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const LoginPage()));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return AppTemplate(
-      title: 'Splash',
-      currentPage: AppPage.splash,
-      page: Center(
+    return Scaffold(
+      body: Center(
         child: Container(
-          height: 200,
-          width: 200,
-          decoration: BoxDecoration(
-            color: Colors.redAccent,
-            borderRadius: BorderRadius.circular(100),
-          ),
-          child: const Center(
-            child: Text(
-              'Pokémon App',
-              style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(color: AppColors.primary),
+          child: Center(
+            child: Image(
+              height: AppSizes(context).height * 0.1,
+              image: AssetImage('assets/branding/applogo.png'),
             ),
           ),
         ),
