3fb8e3b817f5d5e3cf053ba8c44f651aaf957c08
Xolani <xr.mkhwanazi.20@gmail.com>
2026-03-22 13:37:50 +0200

    Change Flutter Flash Bar To Toast Bar

--- lib/widgets/app_toast.dart
+++ lib/widgets/app_toast.dart
@@ -1,32 +1,26 @@
-import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:king_price_pokemon_application/helpers/app_colors.dart';
 import 'package:king_price_pokemon_application/helpers/app_sizes.dart';
 
-Flushbar showSuccessToast(
-  BuildContext context,
-  String message, {
-  Duration duration = const Duration(seconds: 4),
-}) {
-  return Flushbar<void>(
-    title: 'Success',
-    titleSize: AppSizes(context).font.large,
-    messageSize: AppSizes(context).font.medium,
-    message: message,
-    duration: duration,
+void showSuccessToast(BuildContext context, String message) {
+  Fluttertoast.showToast(
+    msg: message,
+    toastLength: Toast.LENGTH_LONG,
+    gravity: ToastGravity.BOTTOM,
     backgroundColor: AppColors.successToast,
-    onTap: (flushbar) => flushbar.dismiss(),
-  )..show(context);
+    textColor: Colors.white,
+    fontSize: AppSizes(context).font.small,
+  );
 }
 
-Flushbar showErrorToast(BuildContext context, String message, {Duration? duration}) {
-  return Flushbar<void>(
-    title: 'Error',
-    titleSize: AppSizes(context).font.large,
-    messageSize: AppSizes(context).font.medium,
-    message: message,
-    duration: duration ?? const Duration(seconds: 4),
+void showErrorToast(BuildContext context, String message) {
+  Fluttertoast.showToast(
+    msg: message,
+    toastLength: Toast.LENGTH_LONG,
+    gravity: ToastGravity.BOTTOM,
     backgroundColor: AppColors.errorToast,
-    onTap: (flushbar) => flushbar.dismiss(),
-  )..show(context);
+    textColor: Colors.black,
+    fontSize: AppSizes(context).font.small,
+  );
 }
