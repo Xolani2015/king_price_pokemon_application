e1d8bce37e6c0a9133d0c77c87e43ad752fb5e71
Xolani <xr.mkhwanazi.20@gmail.com>
2026-03-22 13:45:14 +0200

    Create Pokemon Service

---
+++ lib/services/pokemon_api_service.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:king_price_pokemon_application/models/pokemon_model.dart';
+
+class PokemonService {
+  final String baseUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+  Future<List<Pokemon>> fetchPokemon({int offset = 0, int limit = 20}) async {
+    final response = await http.get(Uri.parse('$baseUrl?offset=$offset&limit=$limit'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final results = List<Map<String, dynamic>>.from(data['results']);
+
+      final detailedResults = await Future.wait(
+        results.map((poke) async {
+          final detailRes = await http.get(Uri.parse(poke['url']));
+          if (detailRes.statusCode == 200) {
+            final detailData = json.decode(detailRes.body);
+            return Pokemon.fromJson(detailData);
+          }
+          return Pokemon(name: poke['name'], image: '', hp: 0, attack: 0, defense: 0, speed: 0);
+        }),
+      );
+
+      return detailedResults;
+    } else {
+      throw Exception('Failed to load Pokémon');
+    }
+  }
+}
